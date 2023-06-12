13e5b647b22b8de2480e29c63952e2b752ecca03
Валентина Валентина <veborovskaya@gmail.com>
2023-06-12 23:08:51 +0300

    ----

---
+++ AuthorizationServer/AuthorizationServer/Users.cs
@@ -0,0 +1,26 @@
+﻿namespace AuthorizationServer
+{
+
+    public class Users
+    {
+        public static List<User> ListUsers = new List<User>
+        {
+            new User 
+            { 
+                Id = 1, 
+                Name = "Alice", 
+                Email="Alice@com", 
+                Password="1234", 
+                Role = Roles.Manager 
+            },
+            new User
+            {
+                Id = 2,
+                Name = "Bob",
+                Email="Bob@com",
+                Password="1234",
+                Role = Roles.Buyer
+            }
+        };
+    }
+}
